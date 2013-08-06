20f2c1162381b864f57b8d65db6584d7fa438cfd
alfonso-garcia <alfonso.garcia@cubilabs.com>
2013-08-06 15:06:05 +0000

    data from index.html

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    var data = fs.readFileSync('./index.html','utf8');
+    response.send(data);
 });
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
